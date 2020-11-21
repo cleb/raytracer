6691b94f337715f683671254c3728c027b813aa7
jakub <jakub@judas.cz>
2020-11-21 11:55:47 +0100

    Intersection tests

--- test/test_raytracer.c
+++ test/test_raytracer.c
@@ -11,7 +11,20 @@ START_TEST(test_intersects_direct)
     Angle alpha = {.angle = M_PI_2, .cos = 0, .sin = 1, .tg= INFINITY};
     Angle beta = {.angle = 0, .cos = 1, .sin = 0, .tg= 0};
     Intersection intersection = intersects(0,0,0,alpha,beta, &wall);
+    ck_assert_int_eq(1,intersection.distance != INFINITY);
+}
+END_TEST
 
+START_TEST(test_intersects_miss)
+{
+    Point p1 = {.x = -100, .y = 10};
+    Point p2 = {.x = 100, .y = 10};
+    Wall wall_data = {.p1 = p1, .p2 = p2, .bottom = -100, .top = 100};
+    Render_Wall wall = {.a = 0, .b = 10, .wall = &wall_data};
+    Angle alpha = {.angle = 0, .cos = 1, .sin = 0, .tg= 0}; 
+    Angle beta = {.angle = 0, .cos = 1, .sin = 0, .tg= 0};
+    Intersection intersection = intersects(0,0,0,alpha,beta, &wall);
+    ck_assert_int_eq(1,INFINITY == intersection.distance);
 }
 END_TEST
 

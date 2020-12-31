c01f18fab073e5ed8279d6d6e076d7cc62961cf6
jakub <jakub@judas.cz>
2020-12-31 15:07:48 +0100

    Free texture surfaces

--- src/texture_loader.c
+++ src/texture_loader.c
@@ -3,19 +3,21 @@
 #include "./color.h"
 #include "./texture.h"
 
-Texture *load_texture(const char* filename) {
+Texture *load_texture(const char *filename)
+{
     SDL_Surface *image = IMG_Load(filename);
     SDL_PixelFormat *format = SDL_AllocFormat(SDL_PIXELFORMAT_RGBA32);
     SDL_Surface *image_rgba = SDL_ConvertSurface(image, format, 0);
-    SDL_FreeFormat(format);
 
-    Texture * ret = (Texture *)malloc(sizeof(Texture));
+    Texture *ret = (Texture *)malloc(sizeof(Texture));
     ret->w = image_rgba->w;
     ret->h = image_rgba->h;
     ret->pixels = (Color *)malloc(ret->h * ret->w * sizeof(Color));
-    unsigned char * pixels = image_rgba->pixels;
-    for(int y = 0; y < ret->h; y ++) {
-        for(int x = 0; x < ret->w; x ++) {
+    unsigned char *pixels = image_rgba->pixels;
+    for (int y = 0; y < ret->h; y++)
+    {
+        for (int x = 0; x < ret->w; x++)
+        {
             ret->pixels[y * ret->w + x].r = pixels[(y * ret->w + x) * 4];
             ret->pixels[y * ret->w + x].g = pixels[(y * ret->w + x) * 4 + 1];
             ret->pixels[y * ret->w + x].b = pixels[(y * ret->w + x) * 4 + 2];
@@ -23,9 +25,14 @@ Texture *load_texture(const char* filename) {
             ret->pixels[y * ret->w + x].alpha_double = ret->pixels[y * ret->w + x].alpha / 255.0f;
         }
     }
-    return ret;        
+
+    SDL_FreeFormat(format);
+    SDL_FreeSurface(image_rgba);
+    SDL_FreeSurface(image);
+    return ret;
 }
 
-Texture *destroy_texture(Texture *texture){
+Texture *destroy_texture(Texture *texture)
+{
     free(texture);
 }
